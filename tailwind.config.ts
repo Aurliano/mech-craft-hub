08a013a6ca5100a6549eca58f0bac33e76ba59be
Ali Kazemzadeh <kazemzadehalik@gmail.com>
2025-08-13 16:44:53 +0330

    Updated tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,7 @@ export default {
 		},
 		extend: {
 			fontFamily: {
-				sans: ['Yekan' , 'sans-serif'],
+				sans: ['Yekan' , sans-serif],
 			},
 			colors: {
 				border: 'hsl(var(--border))',
